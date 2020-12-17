24af3c70218245de4aa60355b2f62d4cee62ff4c
shijie-l <59986979+shijie-l@users.noreply.github.com>
2020-12-17 17:13:48 +0800

    Unit Stats now scale with Jon's leveling formula

---
+++ Assets/_PROJECT/Scripts/SO Scripts/UnitObject.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Unit/Unit Object")]
+public class UnitObject : ScriptableObject
+{
+    [Header("General")]
+    public string ID;
+    [Range(3, 5)] public int BaseRarity;
+    public int MaxLevel;
+    public int MaxHealth;
+    public int MaxAttack;
+    public int MaxDefence;
+    public string CharacterDescription;
+
+    [Header("Skill")]
+    //public Skill UnitSkill;
+    [TextArea(1, 5)] public string SkillDescription;
+
+    [Header("UI/Visual")]
+    public Image SplashArt;
+    public Image PortraitArt;
+}
