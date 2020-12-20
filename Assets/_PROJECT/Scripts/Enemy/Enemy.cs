9662c1820ddd8abbaf709d26f7cee96e52cfc524
jinzhihoi <56183521+jinzhihoi@users.noreply.github.com>
2020-12-20 19:24:51 +0800

    enemy spawn yay
    xd

--- Assets/_PROJECT/Scripts/Enemy/Enemy.cs
+++ Assets/_PROJECT/Scripts/Enemy/Enemy.cs
@@ -12,14 +12,22 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int maxLoadOutSize;
     [SerializeField] private GameObject inventoryHolder;
     [SerializeField] private List<UnitEntry> loadOut;
-
+    [SerializeField] private List<UnitEntry> enemyLibrary;
     public int Experience => _experience;
     public int Level => _level;
     public List<UnitEntry> UnitLoadOut => loadOut;
-
+    public List<UnitEntry> EnemyLibrary => enemyLibrary;
     private void Awake()
     {
         if (Instance != null) Destroy(gameObject);
         else Instance = this;
+    }
+
+    private void Start()
+    {
+        for(int i = 0; i < loadOut.Count; i++)
+        {
+            loadOut[i] = enemyLibrary[Random.Range(0, enemyLibrary.Count)];
+        }
     }
 }
