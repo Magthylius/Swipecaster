bdae275a0b48a128b8c616537371c8dd039be2d4
Magthylius <magthylius@gmaillcom>
2020-12-30 15:06:26 +0800

    creating inventory

---
+++ Assets/_PROJECT/Scripts/Main Menu/InventoryManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MenuCanvasPage
+{
+    DatabaseManager databaseManager;
+
+    void Start()
+    {
+        databaseManager = DatabaseManager.instance;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
