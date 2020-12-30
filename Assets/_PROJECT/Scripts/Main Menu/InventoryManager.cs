002ad8125a314c3882effa230d2d8525ca0fc305
Magthylius <magthylius@gmaillcom>
2020-12-30 15:27:02 +0800

    tweaking invmanager

--- Assets/_PROJECT/Scripts/Main Menu/InventoryManager.cs
+++ Assets/_PROJECT/Scripts/Main Menu/InventoryManager.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class InventoryManager : MenuCanvasPage
 {
     DatabaseManager databaseManager;
+    PlayerInventory playerInventory;
 
     void Start()
     {
         databaseManager = DatabaseManager.instance;
+        playerInventory = PlayerInventory.instance;
     }
-
 
     void Update()
     {
