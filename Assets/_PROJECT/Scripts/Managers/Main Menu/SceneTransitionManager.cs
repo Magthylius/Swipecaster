3caf764c7bf8884fa8c7c8ba195c828081ecb199
Magthylius <magthylius@gmaillcom>
2021-01-01 22:58:21 +0800

    trying to implement scene transition manager

---
+++ Assets/_PROJECT/Scripts/Managers/Main Menu/SceneTransitionManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using LerpFunctions;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public static SceneTransitionManager instance;
+
+    public float transitionSpeed = 10f;
+    public List<GameObject> loadingObjects;
+
+    CanvasGroupFader canvasCG;
+    string targetScene;
+
+    AsyncOperation asyncLoad;
+
+    void OnEnable()
+    {
+        
+    }
+
+    void Awake()
+    {
+        if (instance != null)
+            Destroy(this);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        canvasCG = new CanvasGroupFader(gameObject.GetComponent<CanvasGroup>(), true, true);
+        canvasCG.fadeEndedEvent.AddListener(ShowLoaders);
+
+        SceneManager.sceneLoaded += OnSceneLoad;
+        DeactivateTransition();
+    }
+
+    void Update()
+    {
+        canvasCG.Step(transitionSpeed * Time.unscaledDeltaTime);
+        if (Time.unscaledDeltaTime >= 0.01f) print(Time.unscaledDeltaTime); 
+    }
+
+    void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        //StartCoroutine(Delay(1f));
+        while (!asyncLoad.isDone)
+        {
+
+        }
+
+        DeactivateTransition();
+    }
+
+    public void ActivateTransition(string transitionTarget)
+    {
+        canvasCG.StartFadeIn();
+        targetScene = transitionTarget;
+    }
+
+    void DeactivateTransition()
+    {
+        SetObjectActives(false);
+        canvasCG.StartFadeOut();
+    }
+
+    void SetObjectActives(bool activation)
+    {
+        foreach (GameObject obj in loadingObjects) obj.SetActive(activation);
+    }
+
+    void ShowLoaders()
+    {
+        if (canvasCG.State == CanvasGroupFader.CanvasState.FADE_IN)
+        {
+            SetObjectActives(true);
+            asyncLoad = SceneManager.LoadSceneAsync(targetScene);
+        }
+    }
+
+    IEnumerator Delay(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }    
+}
