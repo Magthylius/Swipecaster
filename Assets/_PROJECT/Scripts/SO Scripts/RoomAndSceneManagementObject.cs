67a49977d107c8a4ed1fdca52914e043987b3c66
shijie-l <59986979+shijie-l@users.noreply.github.com>
2021-01-01 19:06:55 +0800

    energy deduction from level selection

--- Assets/_PROJECT/Scripts/SO Scripts/RoomAndSceneManagementObject.cs
+++ Assets/_PROJECT/Scripts/SO Scripts/RoomAndSceneManagementObject.cs
@@ -7,19 +7,25 @@ using UnityEngine.SceneManagement;
 public class RoomAndSceneManagementObject : ScriptableObject
 {
     private const string _roomLocation = "ScriptableObjects/Enemy/Rooms";
+    private static bool _isInitialised = false;
     private static List<RoomScriptable> _resourceRooms = new List<RoomScriptable>();
     private static Dictionary<string, RoomScriptable> _rooms = new Dictionary<string, RoomScriptable>();
-    
+    private static EnergyManager _energyManager = null;
+
     private const int MainMenuIndex = 0;
     private const int BattleStageIndex = 1;
 
+    [SerializeField] private float smallEnergyCost;
+    [SerializeField] private float mediumEnergyCost;
+    [SerializeField] private float highEnergyCost;
+    [SerializeField] private float superHighEnergyCost;
     public RoomScriptable ActiveRoom;
 
     #region Level & Room Selection
 
-    public void Stage1Room1() => EnterStage(nameof(Stage1Room1));
-    public void Stage1Room2() => EnterStage(nameof(Stage1Room2));
-    public void Stage1Room3() => EnterStage(nameof(Stage1Room3));
+    public void Stage1Room1() => EnterStage(nameof(Stage1Room1), smallEnergyCost);
+    public void Stage1Room2() => EnterStage(nameof(Stage1Room2), smallEnergyCost);
+    public void Stage1Room3() => EnterStage(nameof(Stage1Room3), smallEnergyCost);
 
     #endregion
 
@@ -29,21 +35,29 @@ public class RoomAndSceneManagementObject : ScriptableObject
         SceneManager.LoadScene(MainMenuIndex, LoadSceneMode.Single);
     }
 
-    private void EnterStage(string name)
+    private void EnterStage(string name, float energy)
     {
-        InitialiseRoomResources();
+        InitialiseIfAny();
         ActiveRoom = GetRoom(name, out var isActive);
-        if(isActive) SceneManager.LoadScene(BattleStageIndex, LoadSceneMode.Single);
+        if (!isActive || _energyManager == null) return;
+
+        _energyManager.SpendEnergy(Mathf.Abs(energy));
+        SceneManager.LoadScene(BattleStageIndex, LoadSceneMode.Single);
     }
     private RoomScriptable GetRoom(string name, out bool active)
     {
         active = _rooms.TryGetValue(name, out var value);
         return value;
     }
-    private void InitialiseRoomResources()
+    private void InitialiseIfAny()
     {
-        if (_resourceRooms.Count != 0) return;
+        if (!NotInitialised) return;
+        _isInitialised = true;
+
         _resourceRooms = new List<RoomScriptable>(Resources.LoadAll<RoomScriptable>(_roomLocation));
         _resourceRooms.ForEach(room => _rooms.Add(room.name, room));
+        _energyManager = EnergyManager.instance;
     }
+
+    private bool NotInitialised => !_isInitialised;
 }
