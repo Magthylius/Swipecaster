0ef0f8dce57241cd1c1b3df6f8783a74f5115f22
Magthylius <magthylius@gmaillcom>
2021-01-05 20:06:24 +0800

    updating inventory with gacha pull and reset button

---
+++ Assets/_PROJECT/Scripts/Main Menu/HomeCanvasManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LerpFunctions;
+
+public class HomeCanvasManager : MenuCanvasPage
+{
+    public static HomeCanvasManager instance;
+    DatabaseManager dataManager;
+    InventoryManager invManager;
+
+    public CanvasGroup resetCanvasGroup;
+    public float transitionSpeed = 5f;
+
+    CanvasGroupFader resetCGF;
+
+    public override void Awake()
+    {
+        base.Awake();
+        if (instance != null) Destroy(this);
+        else instance = this; 
+    }
+
+    void Start()
+    {
+        dataManager = DatabaseManager.instance;
+        invManager = InventoryManager.instance;
+
+        resetCGF = new CanvasGroupFader(resetCanvasGroup, true, true);
+        resetCGF.SetTransparent();
+    }
+
+    void Update()
+    {
+        resetCGF.Step(transitionSpeed * Time.unscaledDeltaTime);
+    }
+
+    public override void Reset()
+    {
+        resetCGF.SetTransparent();
+        resetCGF.SetStateFadeIn();
+    }
+
+    public void BTN_ShowResetCanvas()
+    {
+        resetCGF.StartFadeIn();
+    }
+
+    public void BTN_HideResetCanvas(bool resetData)
+    {
+        if (resetData)
+        {
+            dataManager.GenerateNewSaveData();
+            invManager.UpdateCasterInventory();
+        }
+
+        resetCGF.StartFadeOut();
+    }
+}
