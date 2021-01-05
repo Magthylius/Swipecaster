ff9f39b4290c2da7b03d656447ab67bb657b907f
jinzhihoi <56183521+jinzhihoi@users.noreply.github.com>
2021-01-05 21:54:29 +0800

    damagePopUp leggo
    xd

--- Assets/_PROJECT/Scripts/UI/DamagePopUp.cs
+++ Assets/_PROJECT/Scripts/UI/DamagePopUp.cs
@@ -8,13 +8,23 @@ public class DamagePopUp : MonoBehaviour
     private float popUpTime = 2.0f;
     private TextMeshPro _textMesh;
     private GameObject parent;
+    private GameObject offsetObj;
 
     private void Awake()
     {
         _textMesh = GetComponent<TextMeshPro>();
         parent = transform.parent.gameObject;
-        parent.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
         parent.SetActive(false);
+        
+        //create parent to offset
+        offsetObj = new GameObject("PopUpOffset");
+        offsetObj.transform.position = parent.transform.position;
+        offsetObj.transform.SetParent(parent.transform.parent);
+        parent.transform.SetParent(offsetObj.transform);
+        offsetObj.transform.localScale = new Vector3(1, 1, 1);
+
+        //offset
+        offsetObj.transform.localPosition -= new Vector3(2.0f, 0.0f, 0f);
     }
 
     public void ShowDamage(int damage) => StartCoroutine(DoPopUp(damage));
@@ -25,5 +35,10 @@ public class DamagePopUp : MonoBehaviour
         _textMesh.text = damage.ToString();
         yield return new WaitForSeconds(popUpTime);
         parent.SetActive(false);
+    }
+
+    private Transform GetUnit()
+    {
+        return transform.parent.parent.parent;
     }
 }
