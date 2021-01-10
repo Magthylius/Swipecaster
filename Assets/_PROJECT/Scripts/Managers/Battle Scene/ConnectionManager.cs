964bf5a735f76f13eacfeae3b15699db6883b88d
Magthylius <magthylius@gmaillcom>
2021-01-10 17:00:55 +0800

    tweak to fix rune conencting not center

--- Assets/_PROJECT/Scripts/Managers/Battle Scene/ConnectionManager.cs
+++ Assets/_PROJECT/Scripts/Managers/Battle Scene/ConnectionManager.cs
@@ -36,81 +36,6 @@ public class ConnectionManager : MonoBehaviour
 
     void Update()
     {
-        /*
-        if (Input.GetMouseButton(0))
-        {
-            if (EventSystem.current.IsPointerOverGameObject())
-            {
-                return;
-            }
-            
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
-
-            if (hit.collider != null)
-            {
-                if (hit.collider.CompareTag("Rune1"))
-                {
-                    print(hit.collider.name);
-                    Transform tempTransform = hit.collider.transform;
-                    RuneBehaviour tempRB = tempTransform.GetComponent<RuneBehaviour>();
-
-                    if (!tempRB.GetSelected())
-                    {
-                        
-                        tempRB.Selected();
-                        line.gameObject.SetActive(true);
-                    }
-
-                    if (selectionList.Count >= 2)
-                    {
-                        if (!comboManager.GetIsStart())
-                        {
-                            comboManager.SetCountdownTimer();
-                            comboManager.SetIsStart();
-                        }
-                    }
-                }
-            }
-
-        }
-
-
-        if (selectionStarted)
-        {
-            Vector3[] posList = new Vector3[selectionList.Count];
-
-            for (int i = 0; i < selectionList.Count; i++)
-            {
-                Vector2 temp = selectionList[i].GetPosition();
-
-                
-                Vector3 temp2 = new Vector3(temp.x, temp.y, -5);
-
-                posList[i] = temp2;
-            }
-
-            line.positionCount = selectionList.Count;
-            line.SetPositions(posList);
-
-            if (Input.GetMouseButtonUp(0))
-            {
-                selectionStarted = false;
-                Time.timeScale = 1.0f;
-                line.gameObject.SetActive(false);
-
-                if (selectionList.Count >= 2)
-                {
-                    comboManager.CollectDamage();
-                }
-
-                foreach (var rune in selectionList)
-                {
-                    rune.GetComponent<RuneBehaviour>().ResetToActivateSprite();
-                }
-            }
-        } */
-
         if (selectionStarted)
         {
             if (selectionList.Count >= 2)
@@ -160,7 +85,7 @@ public class ConnectionManager : MonoBehaviour
         foreach (RuneBehaviour rune in selectionList)
         {
             Vector2 pos = rune.runeCenter;
-            pos.y += uiLine.gridSize.y * 0.5f - runeHalfWidth;
+            pos.y += uiLine.gridSize.y * 0.5f;
             linePosList.Add(pos);
             //print(pos);
         }
