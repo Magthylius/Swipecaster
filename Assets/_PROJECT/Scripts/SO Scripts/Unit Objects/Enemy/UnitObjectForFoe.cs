5e9aa26f15986cf69353a8fa4255f23a7dafb54f
shijie-l <59986979+shijie-l@users.noreply.github.com>
2021-01-13 00:37:24 +0800

    (UnitObjectForFoe.cs) Wait they should be integers

--- Assets/_PROJECT/Scripts/SO Scripts/Unit Objects/Enemy/UnitObjectForFoe.cs
+++ Assets/_PROJECT/Scripts/SO Scripts/Unit Objects/Enemy/UnitObjectForFoe.cs
@@ -1,24 +1,23 @@
-using ConversionFunctions;
 using UnityEngine;
 
 public class UnitObjectForFoe : UnitObject
 {
     [Header("Foe Settings")]
-    [SerializeField, Range(1.0f, 5000.0f)] private float minRandomHealth;
-    [SerializeField, Range(1.0f, 5000.0f)] private float maxRandomHealth;
+    [SerializeField, Range(1, 5000)] private int minRandomHealth;
+    [SerializeField, Range(1, 5000)] private int maxRandomHealth;
     [Space(5)]
-    [SerializeField, Range(1.0f, 5000.0f)] private float minRandomAttack;
-    [SerializeField, Range(1.0f, 5000.0f)] private float maxRandomAttack;
+    [SerializeField, Range(1, 5000)] private int minRandomAttack;
+    [SerializeField, Range(1, 5000)] private int maxRandomAttack;
     [Space(5)]
-    [SerializeField, Range(1.0f, 5000.0f)] private float minRandomDefence;
-    [SerializeField, Range(1.0f, 5000.0f)] private float maxRandomDefence;
+    [SerializeField, Range(1, 5000)] private int minRandomDefence;
+    [SerializeField, Range(1, 5000)] private int maxRandomDefence;
 
     public override void CalculateRandomisedStats()
     {
         if (!MinIsLowerThanMax()) { DebugMinIsLargerThanMaxCase(); return; }
-        int randomHealth = Random.Range(minRandomHealth, maxRandomHealth).AsInt();
-        int randomAttack = Random.Range(minRandomAttack, maxRandomAttack).AsInt();
-        int randomDefence = Random.Range(minRandomDefence, maxRandomDefence).AsInt();
+        int randomHealth = Random.Range(minRandomHealth, maxRandomHealth + 1);
+        int randomAttack = Random.Range(minRandomAttack, maxRandomAttack + 1);
+        int randomDefence = Random.Range(minRandomDefence, maxRandomDefence + 1);
         CalculateActualStats(randomHealth, randomAttack, randomDefence);
     }
 
