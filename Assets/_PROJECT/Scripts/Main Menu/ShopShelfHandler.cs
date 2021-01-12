8765860cbfd09b3842d21a708657e63810931658
Magthylius <magthylius@gmaillcom>
2021-01-13 00:12:32 +0800

    swapped shopbutton

--- Assets/_PROJECT/Scripts/Main Menu/ShopShelfHandler.cs
+++ Assets/_PROJECT/Scripts/Main Menu/ShopShelfHandler.cs
@@ -13,12 +13,39 @@ public class ShopShelfHandler : MonoBehaviour
         for (int i = 0; i < shopDataList.Count; i++)
         {
             GameObject shelf = Instantiate(shopObjectPrefab, shopParent);
-            shelf.GetComponent<ShopButtonBehavior>().SetupImmediate(shopDataList[i]);
+            ShopButtonBehavior behav = shelf.GetComponent<ShopButtonBehavior>();
+            behav.SetupImmediate(shopDataList[i], this);
+
+            /*if (shopDataList[i].shopType == CurrencyType.NORMAL_CURRENCY) behav.button.onClick.AddListener(PurchaseCash);
+            else if (shopDataList[i].shopType == CurrencyType.PREMIUM_CURRENCY) behav.button.onClick.AddListener(PurhaseGems);
+
+            if (shopDataList[i].buyType == CurrencyType.NORMAL_CURRENCY) behav.button.onClick.AddListener(DeductCash);
+            else if (shopDataList[i].buyType == CurrencyType.PREMIUM_CURRENCY) behav.button.onClick.AddListener(DeductGems);*/
         }
         
     }
     void Update()
     {
         
+    }
+
+    public void PurhaseGems(int amt)
+    {
+
+    }
+
+    public void PurchaseCash(int amt)
+    {
+
+    }
+
+    public void DeductGems(int amt)
+    {
+
+    }
+
+    public void DeductCash(int amt)
+    {
+
     }
 }
